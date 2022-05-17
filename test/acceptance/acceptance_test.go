9f0a4430446746e7afb2ec80b88a80dc57f44ed6
Emanuele Falzone <emafalzo@gmail.com>
2022-05-17 16:18:34 +0200

    Link the feature steps to the corresponding methods

---
+++ test/acceptance/acceptance_test.go
@@ -0,0 +1,29 @@
+package acceptance_test
+
+import (
+	"github.com/cucumber/godog"
+	"github.com/emanuelefalzone/bitly/test/acceptance/client"
+)
+
+func InitializeScenario(fn func() *client.Client) func(*godog.ScenarioContext) {
+	return func(ctx *godog.ScenarioContext) {
+		client := fn()
+
+		// GIVEN
+		ctx.Step(`^that I got a short link for (.*)$`, client.CreateRedirection)
+		ctx.Step(`^that I got a short link that does not exist$`, client.GetNonExistingKey)
+		ctx.Step(`^that the link has been visited (\d+) times$`, client.GetRedirectionLocationTimes)
+
+		// WHEN
+		ctx.Step(`^I command the system to shorten the link (.*)$`, client.CreateRedirection)
+		ctx.Step(`^I command the system to delete the short link$`, client.DeleteRedirection)
+		ctx.Step(`^I navigate to the short link$`, client.GetRedirectionLocation)
+
+		// THEN
+		ctx.Step(`^the system redirects me to (.*)$`, client.ConfirmLocationToBe)
+		ctx.Step(`^the system returns a short link$`, client.ConfirmHasKey)
+		ctx.Step(`^the system confirms that the operation was succesfully executed$`, client.ConfirmNoError)
+		ctx.Step(`^the system signals that the short link does not exist$`, client.ConfirmError)
+		ctx.Step(`^the system signals that the link is not valid$`, client.ConfirmError)
+	}
+}
