dbe5168357f73a02d990980f51763ecdb3151e9d
Emanuele Falzone <emafalzo@gmail.com>
2022-05-17 17:20:00 +0200

    Add context parameter

--- internal/domain/redirection/repository.go
+++ internal/domain/redirection/repository.go
@@ -1,9 +1,11 @@
 package redirection
 
+import "context"
+
 //go:generate mockgen -destination=../../../test/mock/redirection_repository.go -package=mock -mock_names=Repository=MockRedirectionRepository github.com/emanuelefalzone/bitly/internal/domain/redirection Repository
 
 type Repository interface {
-	Create(Redirection) error
-	Delete(Redirection) error
-	FindByKey(string) (Redirection, error)
+	Create(context.Context, Redirection) error
+	Delete(context.Context, Redirection) error
+	FindByKey(context.Context, string) (Redirection, error)
 }
