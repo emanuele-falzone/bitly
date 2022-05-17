5314c84f038d8a7a892f0795b3fa56293bd0db0b
Emanuele Falzone <emafalzo@gmail.com>
2022-05-17 13:18:40 +0200

    Implement random key generator

---
+++ internal/service/random_key_generator.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"math/rand"
+)
+
+const (
+	// Only use lower case char (minus i,j and l) and digits so that the key is easy to dictate
+	alphabet = "abcdefghkmnopqrstuvwxyz0123456789"
+	// With a 6 chars key we have 1291467969 possible keys
+	// This shuold be enougth, be can be increased if requested
+	length = 6
+)
+
+// Inspired by https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+type RandomKeyGenerator struct {
+	rand *rand.Rand
+}
+
+func NewRandomKeyGenerator(seed int64) KeyGenerator {
+	rand := rand.New(rand.NewSource(seed))
+	return RandomKeyGenerator{rand: rand}
+}
+
+// The Next Key method simply ignores the location parameter and creare a fixed length random key
+func (s RandomKeyGenerator) NextKey(location string) string {
+	b := make([]byte, length)
+	// extract random chars from alphabet
+	for i := range b {
+		b[i] = alphabet[s.rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
---
+++ internal/service/random_key_generator_test.go
@@ -0,0 +1,29 @@
+package service_test
+
+import (
+	"github.com/emanuelefalzone/bitly/internal/service"
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+func TestRandomKeyGenerator_NextKey(t *testing.T) {
+	// Build our needed testcase data struct
+	type testCase struct {
+		location string
+		key      string
+	}
+	// Create new test cases
+	testCases := []testCase{
+		{
+			location: "http://www.google.com",
+			key:      "dd5w3b",
+		},
+	}
+
+	generator := service.NewRandomKeyGenerator(0)
+	for _, tc := range testCases {
+		// Run Tests
+		assert.Equal(t, tc.key, generator.NextKey(tc.location))
+	}
+}
