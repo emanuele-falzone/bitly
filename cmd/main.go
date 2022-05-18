b30c26a0795b4d2cfbab7f3f591e7457bb762852
Emanuele Falzone <emafalzo@gmail.com>
2022-05-18 15:02:44 +0200

    Create and start grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/emanuelefalzone/bitly/internal"
+	"github.com/emanuelefalzone/bitly/internal/adapter/persistence/redis"
+	"github.com/emanuelefalzone/bitly/internal/adapter/service/grpc"
+	"github.com/emanuelefalzone/bitly/internal/application"
+	"github.com/emanuelefalzone/bitly/internal/service"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	log.Println(`Starting`)
+
+	redisConnectionString, err := internal.GetEnv("REDIS_CONNECTION_STRING")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcPort, err := internal.GetEnv("GRPC_PORT")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redirectionRepository, err := redis.NewRedirectionRepository(redisConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	keyGenerator := service.NewRandomKeyGenerator(time.Now().Unix())
+
+	app := application.New(redirectionRepository, keyGenerator)
+
+	intGrpcPort, err := strconv.Atoi(grpcPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer(app)
+	grpcServer.Start(intGrpcPort)
 }
