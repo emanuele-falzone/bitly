679b53e15c0c2016e90e4a7c9b2bb7525a883606
Emanuele Falzone <emafalzo@gmail.com>
2022-05-18 12:04:24 +0200

    Refactor acceptance tests with go driver

---
+++ test/acceptance/go_acceptance_test.go
@@ -0,0 +1,92 @@
+package acceptance_test
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+	"github.com/emanuelefalzone/bitly/internal"
+	"github.com/emanuelefalzone/bitly/internal/adapter/persistence/memory"
+	"github.com/emanuelefalzone/bitly/internal/adapter/persistence/redis"
+	"github.com/emanuelefalzone/bitly/internal/application"
+	"github.com/emanuelefalzone/bitly/internal/service"
+	"github.com/emanuelefalzone/bitly/test/acceptance/client"
+	"github.com/emanuelefalzone/bitly/test/acceptance/driver"
+	"github.com/emanuelefalzone/bitly/test/acceptance/scenario"
+)
+
+/*
+This is the most important acceptance test.
+It allows for testing the application directly using golang application code
+In this way we ensure that no business logic has leaked out to any service adapter
+*/
+
+func TestAcceptance_GoDriver_InMemoryRepository(t *testing.T) {
+
+	ctx := context.Background()
+
+	var opts = godog.Options{
+		Format:   "pretty",
+		Output:   colors.Colored(os.Stdout),
+		Paths:    []string{"./feature"},
+		TestingT: t,
+	}
+
+	redirectionRepository := memory.NewRedirectionRepository()
+	keyGenerator := service.NewRandomKeyGenerator(time.Now().Unix())
+	application_ := application.New(redirectionRepository, keyGenerator)
+	driver_ := driver.NewGoDriver(application_)
+
+	status := godog.TestSuite{
+		Name: "Acceptance tests using go driver and in memory repository",
+		ScenarioInitializer: scenario.Initialize(func() *client.Client {
+			return client.NewClient(driver_, ctx)
+		}),
+		Options: &opts,
+	}.Run()
+
+	if status != 0 {
+		os.Exit(status)
+	}
+}
+
+func TestAcceptance_GoDriver_RedisRepository(t *testing.T) {
+
+	ctx := context.Background()
+
+	var opts = godog.Options{
+		Format:   "pretty",
+		Output:   colors.Colored(os.Stdout),
+		Paths:    []string{"./feature"},
+		TestingT: t,
+	}
+
+	connectionString, err := internal.GetEnv("ACCEPTANCE_REDIS_CONNECTION_STRING")
+	if err != nil {
+		panic(err)
+	}
+
+	redirectionRepository, err := redis.NewRedirectionRepository(connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	keyGenerator := service.NewRandomKeyGenerator(time.Now().Unix())
+	application_ := application.New(redirectionRepository, keyGenerator)
+	driver_ := driver.NewGoDriver(application_)
+
+	status := godog.TestSuite{
+		Name: "Acceptance tests using go driver and redis repository",
+		ScenarioInitializer: scenario.Initialize(func() *client.Client {
+			return client.NewClient(driver_, ctx)
+		}),
+		Options: &opts,
+	}.Run()
+
+	if status != 0 {
+		os.Exit(status)
+	}
+}
