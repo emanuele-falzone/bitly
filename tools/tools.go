d91cda8d9df7fe44878d06ff8941f3a362a1b349
Emanuele Falzone <emafalzo@gmail.com>
2022-05-18 21:41:16 +0200

    Require tools for code and doc generation

---
+++ tools/tools.go
@@ -0,0 +1,8 @@
+package tools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
