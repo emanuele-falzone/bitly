6c83f44f3120a2fa3607efcfbe93b23a9c6c9c13
Emanuele Falzone <emafalzo@gmail.com>
2022-05-22 11:54:46 +0200

    Refactor test using table approach

--- internal/error_test.go
+++ internal/error_test.go
@@ -1,9 +1,10 @@
-//go:build unit
+//--go:build unit
 
 package internal_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,233 +12,180 @@ import (
 	"github.com/emanuelefalzone/bitly/internal"
 )
 
-func TestError_ErrorCodeNil(t *testing.T) {
+func TestError_NilError(t *testing.T) {
 	assert.Equal(t, "", internal.ErrorCode(nil))
-}
-
-func TestError_ErrorCode(t *testing.T) {
-	// Build our needed testcase data struct
-	type testCase struct {
-		test         string
-		code         string
-		expectedCode string
-	}
-	// Create new test cases
-	testCases := []testCase{
-		{
-			test:         "Internal",
-			code:         internal.ErrInternal,
-			expectedCode: internal.ErrInternal,
-		}, {
-			test:         "Conflict",
-			code:         internal.ErrConflict,
-			expectedCode: internal.ErrConflict,
-		}, {
-			test:         "NotFound",
-			code:         internal.ErrNotFound,
-			expectedCode: internal.ErrNotFound,
-		}, {
-			test:         "Invalid",
-			code:         internal.ErrInvalid,
-			expectedCode: internal.ErrInvalid,
-		}, {
-			test:         "Internal default",
-			code:         "",
-			expectedCode: internal.ErrInternal,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Code: tc.code}
-			assert.Equal(t, tc.expectedCode, internal.ErrorCode(err))
-		})
-	}
-}
-
-func TestError_ErrorCodeWrap(t *testing.T) {
-	// Build our needed testcase data struct
-	type testCase struct {
-		test         string
-		wrapCode     string
-		expectedCode string
-	}
-	// Create new test cases
-	testCases := []testCase{
-		{
-			test:         "Internal",
-			wrapCode:     internal.ErrInternal,
-			expectedCode: internal.ErrInternal,
-		}, {
-			test:         "Conflict",
-			wrapCode:     internal.ErrConflict,
-			expectedCode: internal.ErrConflict,
-		}, {
-			test:         "NotFound",
-			wrapCode:     internal.ErrNotFound,
-			expectedCode: internal.ErrNotFound,
-		}, {
-			test:         "Invalid",
-			wrapCode:     internal.ErrInvalid,
-			expectedCode: internal.ErrInvalid,
-		}, {
-			test:         "Internal default",
-			wrapCode:     "",
-			expectedCode: internal.ErrInternal,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Err: &internal.Error{Code: tc.wrapCode}}
-			assert.Equal(t, tc.expectedCode, internal.ErrorCode(err))
-		})
-	}
-}
-
-func TestError_ErrorCodeWrapWrap(t *testing.T) {
-	// Build our needed testcase data struct
-	type testCase struct {
-		test         string
-		wrapWrapCode string
-		expectedCode string
-	}
-	// Create new test cases
-	testCases := []testCase{
-		{
-			test:         "Internal",
-			wrapWrapCode: internal.ErrInternal,
-			expectedCode: internal.ErrInternal,
-		}, {
-			test:         "Conflict",
-			wrapWrapCode: internal.ErrConflict,
-			expectedCode: internal.ErrConflict,
-		}, {
-			test:         "NotFound",
-			wrapWrapCode: internal.ErrNotFound,
-			expectedCode: internal.ErrNotFound,
-		}, {
-			test:         "Invalid",
-			wrapWrapCode: internal.ErrInvalid,
-			expectedCode: internal.ErrInvalid,
-		}, {
-			test:         "Internal default",
-			wrapWrapCode: "",
-			expectedCode: internal.ErrInternal,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Err: &internal.Error{Err: &internal.Error{Code: tc.wrapWrapCode}}}
-			assert.Equal(t, tc.expectedCode, internal.ErrorCode(err))
-		})
-	}
-}
-
-func TestError_ErrorMessageNil(t *testing.T) {
 	assert.Equal(t, "", internal.ErrorMessage(nil))
 }
 
-func TestError_ErrorMessageEmpty(t *testing.T) {
+func TestError_EmptyError(t *testing.T) {
 	err := &internal.Error{Err: errors.New("something bad")}
 	assert.Equal(t, "An internal error has occurred. Please contact technical support.", internal.ErrorMessage(err))
+	assert.Equal(t, internal.ErrInternal, internal.ErrorCode(err))
 }
 
-func TestError_ErrorMessage(t *testing.T) {
+func TestError(t *testing.T) {
 	// Build our needed testcase data struct
 	type testCase struct {
 		test            string
+		op              string
+		wraps           int // Number of times the error has been wrapped
+		code            string
 		message         string
+		err             error // Starting err
+		expectedCode    string
 		expectedMessage string
+		expectedError   string
 	}
 	// Create new test cases
 	testCases := []testCase{
+		// Testing different error code
 		{
 			test:            "Internal",
-			message:         "internal error",
-			expectedMessage: "internal error",
+			code:            internal.ErrInternal,
+			message:         "something bad",
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrInternal),
+		}, {
+			test:            "Conflict",
+			code:            internal.ErrConflict,
+			message:         "something bad",
+			expectedCode:    internal.ErrConflict,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrConflict),
+		}, {
+			test:            "NotFound",
+			code:            internal.ErrNotFound,
+			message:         "something bad",
+			expectedCode:    internal.ErrNotFound,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrNotFound),
+		}, {
+			test:            "Invalid",
+			code:            internal.ErrInvalid,
+			message:         "something bad",
+			expectedCode:    internal.ErrInvalid,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrInvalid),
+		}, {
+			test:            "Default",
+			code:            "",
+			message:         "something bad",
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   "something bad",
 		},
-	}
 
-	for _, tc := range testCases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Message: tc.message}
-			assert.Equal(t, tc.expectedMessage, internal.ErrorMessage(err))
-		})
-	}
-}
-
-func TestError_ErrorMessageWrap(t *testing.T) {
-	// Build our needed testcase data struct
-	type testCase struct {
-		test            string
-		wrapMessage     string
-		expectedMessage string
-	}
-	// Create new test cases
-	testCases := []testCase{
+		// Testing different error code with error wrapping
 		{
-			test:            "Internal",
-			wrapMessage:     "internal error",
-			expectedMessage: "internal error",
+			test:            "Internal Wrap 10",
+			wraps:           10,
+			code:            internal.ErrInternal,
+			message:         "something bad",
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrInternal),
+		}, {
+			test:            "Conflict Wrap 10",
+			wraps:           10,
+			code:            internal.ErrConflict,
+			message:         "something bad",
+			expectedCode:    internal.ErrConflict,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrConflict),
+		}, {
+			test:            "NotFound Wrap 10",
+			wraps:           10,
+			code:            internal.ErrNotFound,
+			message:         "something bad",
+			expectedCode:    internal.ErrNotFound,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrNotFound),
+		}, {
+			test:            "Invalid Wrap 10",
+			wraps:           10,
+			code:            internal.ErrInvalid,
+			message:         "something bad",
+			expectedCode:    internal.ErrInvalid,
+			expectedMessage: "something bad",
+			expectedError:   fmt.Sprintf("<%s> something bad", internal.ErrInvalid),
+		}, {
+			test:            "Default Wrap 10",
+			wraps:           10,
+			code:            "",
+			message:         "something bad",
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   "something bad",
 		},
-	}
 
-	for _, tc := range testCases {
-		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Err: &internal.Error{Message: tc.wrapMessage}}
-			assert.Equal(t, tc.expectedMessage, internal.ErrorMessage(err))
-		})
-	}
-}
-
-func TestError_ErrorError(t *testing.T) {
-	// Build our needed testcase data struct
-	type testCase struct {
-		test          string
-		err           error
-		expectedError string
-	}
-	// Create new test cases
-	testCases := []testCase{
+		// Testing with previously existing Error
 		{
-			test:          "Internal",
-			err:           errors.New("something bad"),
-			expectedError: "something bad",
+			test:            "Internal Existing Error",
+			code:            internal.ErrInternal,
+			op:              "Create item in db",
+			message:         "something bad",
+			err:             &internal.Error{Message: "cannot connect to db"},
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   "Create item in db: cannot connect to db",
+		}, {
+			test:            "Conflict Existing Error",
+			code:            internal.ErrConflict,
+			op:              "Create item in db",
+			message:         "something bad",
+			err:             &internal.Error{Message: "cannot connect to db"},
+			expectedCode:    internal.ErrConflict,
+			expectedMessage: "something bad",
+			expectedError:   "Create item in db: cannot connect to db",
+		}, {
+			test:            "NotFound Existing Error",
+			code:            internal.ErrNotFound,
+			op:              "Create item in db",
+			message:         "something bad",
+			err:             &internal.Error{Message: "cannot connect to db"},
+			expectedCode:    internal.ErrNotFound,
+			expectedMessage: "something bad",
+			expectedError:   "Create item in db: cannot connect to db",
+		}, {
+			test:            "Invalid Existing Error",
+			code:            internal.ErrInvalid,
+			op:              "Create item in db",
+			message:         "something bad",
+			err:             &internal.Error{Message: "cannot connect to db"},
+			expectedCode:    internal.ErrInvalid,
+			expectedMessage: "something bad",
+			expectedError:   "Create item in db: cannot connect to db",
+		}, {
+			test:            "Default Existing Error",
+			code:            "",
+			op:              "Create item in db",
+			message:         "something bad",
+			err:             &internal.Error{Message: "cannot connect to db"},
+			expectedCode:    internal.ErrInternal,
+			expectedMessage: "something bad",
+			expectedError:   "Create item in db: cannot connect to db",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.test, func(t *testing.T) {
-			err := &internal.Error{Err: tc.err}
+			// Create starting error
+			err := &internal.Error{Code: tc.code, Message: tc.message, Err: tc.err, Op: tc.op}
+
+			// Wrap error wraps times
+			for i := 0; i < tc.wraps; i++ {
+				err = &internal.Error{Err: err}
+			}
+
+			// Assert expected error code
+			assert.Equal(t, tc.expectedCode, internal.ErrorCode(err))
+
+			// Assert expected error message
+			assert.Equal(t, tc.expectedMessage, internal.ErrorMessage(err))
+
+			// Assert expected error string
 			assert.Equal(t, tc.expectedError, err.Error())
 		})
 	}
-}
-
-func TestError_ErrorErrorWrap(t *testing.T) {
-
-	first := errors.New("something bad happened")
-	err := internal.Error{Op: "test", Err: first}
-
-	assert.Equal(t, "test: something bad happened", err.Error())
-}
-
-func TestError_ErrorErrorWrapWrap(t *testing.T) {
-
-	first := errors.New("something bad happened")
-	second := &internal.Error{Op: "write", Err: first}
-	err := &internal.Error{Op: "test", Err: second}
-
-	assert.Equal(t, "test: write: something bad happened", err.Error())
-}
-
-func TestError_ErrorErrorWrapWrapWithCode(t *testing.T) {
-
-	first := &internal.Error{Code: internal.ErrInternal, Op: "something bad happened"}
-	second := &internal.Error{Op: "write", Err: first}
-	err := internal.Error{Op: "test", Err: second}
-
-	assert.Equal(t, "test: write: something bad happened: <internal> ", err.Error())
 }
